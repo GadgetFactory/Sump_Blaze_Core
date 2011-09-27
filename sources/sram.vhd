b250bfa15723e3cf154ec03d6d5a1915b060844b
Jack Gassett <jack.gassett@gmail.com>
2011-09-27 15:38:51 -0600

    Working with full depth on 16 channels.

--- sources/sram.vhd
+++ sources/sram.vhd
@@ -58,93 +58,23 @@ entity sram is
 end sram;
 
 
-architecture behavioral of sram is
-
-	component BRAM6k36bit -- SampleRAM
-		port (
-			clka: IN std_logic;
-			wea: IN std_logic_VECTOR(0 downto 0);
-			addra: IN std_logic_VECTOR(12 downto 0);
-			dina: IN std_logic_VECTOR(35 downto 0);
-			douta: OUT std_logic_VECTOR(35 downto 0)
-		);
-	end component;
-
-	attribute box_type : boolean;
-	attribute box_type of BRAM6k36bit: component is true;
-	signal addra : std_logic_vector (12 downto 0) := (others => '0');
-	signal writeSignal : std_logic_vector(0 downto 0);
-	signal bramIn, bramOut : std_logic_vector (35 downto 0);
-
-begin
-	-- static memory configuration 
-	ramCE1 <= not '1';
-	ramUB1 <= not '1';
---	ramLB1 <= not '1';
---	ramCE2 <= not '1';
---	ramUB2 <= not '1';
---	ramLB2 <= not '1';
-
-	-- assign signals
-	ramA <= "00000" & addra;
-	ramWE <= not write;
-	ramOE <= not (not write);
-
-	-- assign signals
-	output <= bramOut(35 downto 16) & ramIO1;
-
-	-- memory io interface state controller
-	bramIn <= input;
-	-- memory io interface state controller
-	process(write, input)
-	begin
-		if write = '1' then
-			ramIO1 <= input(15 downto 0);
---			ramIO1 <= input(35 downto 20);
-		else
-			ramIO1 <= (others => 'Z');
---			ramIO2 <= (others => 'Z');
-		end if;
-	end process;	
-
-	writeSignal(0) <= write;
-
-	-- memory address controller
-	process(clock)
-	begin
-		if rising_edge(clock) then
-			if write = '1' then
-				if addra >= (6*1024) - 1 then
-					addra <= (others => '0');
-				else
-					addra <= addra + 1;
-				end if;
-			elsif read = '1' then
-				if addra = "0" then
-					addra <= std_logic_vector(to_unsigned((6*1024) - 1, addra'length));
-				else
-					addra <= addra - 1;
-				end if;
-			end if;
-		end if;
-	end process;
-
-	 Inst_SampleRAM : BRAM6k36bit
-	 port map (
-		 clka => clock,
-		 wea => writeSignal,
-		 addra => addra,
-		 dina => bramIn,
-		 douta => bramOut
-	 );
-
-end behavioral;
-
-
+--architecture behavioral of sram is
 --
---architecture Behavioral of sram is
+--	component BRAM6k36bit -- SampleRAM
+--		port (
+--			clka: IN std_logic;
+--			wea: IN std_logic_VECTOR(0 downto 0);
+--			addra: IN std_logic_VECTOR(12 downto 0);
+--			dina: IN std_logic_VECTOR(35 downto 0);
+--			douta: OUT std_logic_VECTOR(35 downto 0)
+--		);
+--	end component;
 --
---signal address : std_logic_vector (17 downto 0);
+--	attribute box_type : boolean;
+--	attribute box_type of BRAM6k36bit: component is true;
+--	signal addra : std_logic_vector (12 downto 0) := (others => '0');
+--	signal writeSignal : std_logic_vector(0 downto 0);
+--	signal bramIn, bramOut : std_logic_vector (35 downto 0);
 --
 --begin
 --	-- static memory configuration 
@@ -156,36 +86,106 @@ end behavioral;
 ----	ramLB2 <= not '1';
 --
 --	-- assign signals
---	ramA <= address;
+--	ramA <= "00000" & addra;
 --	ramWE <= not write;
 --	ramOE <= not (not write);
-----   output <= ramIO2 & ramIO1;
---   output <= ramIO1 & "11111111111111111111";	--TODO add BRAM for RLE here.
---	
+--
+--	-- assign signals
+--	output <= bramOut(35 downto 16) & ramIO1;
+--
+--	-- memory io interface state controller
+--	bramIn <= input;
 --	-- memory io interface state controller
 --	process(write, input)
 --	begin
 --		if write = '1' then
-----			ramIO1 <= input(15 downto 0);
---			ramIO1 <= input(35 downto 20);
+--			ramIO1 <= input(15 downto 0);
+----			ramIO1 <= input(35 downto 20);
 --		else
 --			ramIO1 <= (others => 'Z');
 ----			ramIO2 <= (others => 'Z');
 --		end if;
---	end process;
---	
+--	end process;	
+--
+--	writeSignal(0) <= write;
+--
 --	-- memory address controller
 --	process(clock)
 --	begin
 --		if rising_edge(clock) then
 --			if write = '1' then
---				address <= address + 1;
+--				if addra >= (6*1024) - 1 then
+--					addra <= (others => '0');
+--				else
+--					addra <= addra + 1;
+--				end if;
 --			elsif read = '1' then
---				address <= address - 1;
+--				if addra = "0" then
+--					addra <= std_logic_vector(to_unsigned((6*1024) - 1, addra'length));
+--				else
+--					addra <= addra - 1;
+--				end if;
 --			end if;
 --		end if;
 --	end process;
 --
+--	 Inst_SampleRAM : BRAM6k36bit
+--	 port map (
+--		 clka => clock,
+--		 wea => writeSignal,
+--		 addra => addra,
+--		 dina => bramIn,
+--		 douta => bramOut
+--	 );
 --
---end Behavioral;
+--end behavioral;
+
+
+
+architecture Behavioral of sram is
+
+signal address : std_logic_vector (17 downto 0);
+
+begin
+	-- static memory configuration 
+	ramCE1 <= not '1';
+	ramUB1 <= not '1';
+--	ramLB1 <= not '1';
+--	ramCE2 <= not '1';
+--	ramUB2 <= not '1';
+--	ramLB2 <= not '1';
+
+	-- assign signals
+	ramA <= address;
+	ramWE <= not write;
+	ramOE <= not (not write);
+--   output <= ramIO2 & ramIO1;
+   output <= "11111111111111111111" & ramIO1;	--TODO add BRAM for RLE here.
+	
+	-- memory io interface state controller
+	process(write, input)
+	begin
+		if write = '1' then
+			ramIO1 <= input(15 downto 0);
+--			ramIO1 <= input(35 downto 20);
+		else
+			ramIO1 <= (others => 'Z');
+--			ramIO2 <= (others => 'Z');
+		end if;
+	end process;
+	
+	-- memory address controller
+	process(clock)
+	begin
+		if rising_edge(clock) then
+			if write = '1' then
+				address <= address + 1;
+			elsif read = '1' then
+				address <= address - 1;
+			end if;
+		end if;
+	end process;
+
+
+end Behavioral;
 
